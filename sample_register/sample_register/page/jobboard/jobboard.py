c632e31e6206bc250c241cb851bf46245fab8aa3
sbkolate <kolate.sambhajI@gmail.com>
2015-12-08 13:03:27 +0530

    code cleanup [job card creation]

--- sample_register/sample_register/page/jobboard/jobboard.py
+++ sample_register/sample_register/page/jobboard/jobboard.py
@@ -4,35 +4,6 @@ from frappe.utils import cstr,now,add_days
 import json
 
 @frappe.whitelist()
-def get_support_ticket_data(args):
-	"""
-		args:{
-			status: All, Closed, Open, Pending,
-			start: val
-			end: val
-		}
-		Support Ticket Data in following format
-		{
-			"lable":"Issue Status"
-			"data": [["date","Issue 1"], ...,["date","Issue N"]]
-		}
-	"""
-	# frappe.errprint(args)
-	return [{
-				"label":"Open",
-				"data": [[1999, 3.0], [2000, 3.9], [2001, 2.0], [2002, 1.2], [2003, 1.3]]
-			},
-			{
-				"label":"Close",
-				"data": [[1999, 2.0], [2000, 2.9], [2001, 1.0], [2002, 3.2], [2003, 5.3]]
-			},
-			{
-				"label":"Pending",
-				"data": [[1999, 3.9], [2000, 10], [2001, 3.0], [2002, 2.2], [2003, 1.9]]
-			}]
-	pass
-
-@frappe.whitelist()
 def get_sample_data():
 	return {
 	"get_sample_data": frappe.db.sql("""select false, name, customer, type, priority, standards, test_group from `tabSample Entry Register` where job_card_status="" order by name""", as_list=1)
@@ -49,10 +20,6 @@ def get_test_data(test_group):
 	return {
 	"get_test_data": frappe.db.sql("""select name from `tabTest Name` where test_group='%s' order by name"""%(test_group), as_list=1)
 	}
-
-@frappe.whitelist()
-def update_job_card(test_group):
-	frappe.msgprint("Job Card created successfuly for : "+test_group);
 
 @frappe.whitelist()
 def create_job_card(test_group,selectedData,test_list_unicode):
