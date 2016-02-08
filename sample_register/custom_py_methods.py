d3f142d0da81f02e9e92fe4238f4ecf613132c2f
patilsangram <sangram.p@indictranstech.com>
2016-02-08 15:54:06 +0530

    [script : notification for quality inspection to submit]

--- sample_register/custom_py_methods.py
+++ sample_register/custom_py_methods.py
@@ -3,15 +3,15 @@ from frappe.utils import rounded,money_in_words
 from frappe.model.mapper import get_mapped_doc
 from frappe import throw, _
 
-@frappe.whitelist()
-def notify_quality_insp(doc, method):
-	for d in doc.get('items'):		 #Enter inspection date for all items that require inspection
-			if frappe.db.get_value("Item", d.item_code, "inspection_required") and not d.qa_no:
-				frappe.throw(_("Quality Inspection required for Item {0}").format(d.item_code))
-			elif d.qa_no:
-				quality_inspection = frappe.get_doc("Quality Inspection", d.qa_no)
-				if quality_inspection.docstatus != 1:
-					frappe.throw(_("Quality Inspection not Submitted"))
+# @frappe.whitelist()
+# def notify_quality_insp(doc, method):
+# 	for d in doc.get('items'):		 #Enter inspection date for all items that require inspection
+# 			if frappe.db.get_value("Item", d.item_code, "inspection_required") and not d.qa_no:
+# 				frappe.throw(_("Quality Inspection required for Item {0}").format(d.item_code))
+# 			elif d.qa_no:
+# 				quality_inspection = frappe.get_doc("Quality Inspection", d.qa_no)
+# 				if quality_inspection.docstatus != 1:
+# 					frappe.throw(_("Quality Inspection not Submitted"))
 
 @frappe.whitelist()
 def send_inspection_mail(item, inspector, name, verifier):
