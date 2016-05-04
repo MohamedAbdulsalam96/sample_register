90f25c3765629c9aa16d1b0f53ce69b286d7eb96
patilsangram <sangram.p@indictranstech.com>
2016-05-04 15:53:02 +0530

    test_book status update

--- sample_register/sample_register/doctype/job_card_creation/job_card_creation.py
+++ sample_register/sample_register/doctype/job_card_creation/job_card_creation.py
@@ -34,12 +34,13 @@ class JobCardCreation(Document):
 			doc_test_book.type = self.type
 			doc_test_book.priority = self.priority
 			doc_test_book.standards = self.standards
-			# doc_test_book.test = r.test
+			doc_test_book.item_code = r.item_code
 			doc_test_book.test_group = r.test_group
+			# doc_test_book.test = r.test
 			doc_test_book.save()
 			test_book_link="<a href='desk#Form/Test Book/"+doc_test_book.name+"'>"+doc_test_book.name+" </a>"
 			job_link="<a href='desk#Form/Job Card Creation/"+doc_test_book.job_card+"'>"+doc_test_book.job_card+" </a>"
-			frappe.msgprint("For Job Card "+job_link+", Test Book "+test_book_link+ " created for Test "+ r.test)
+			frappe.msgprint("For Job Card "+job_link+", Test Book "+test_book_link+ " created")
 			r.test_book = doc_test_book.name
 		
 	def check_sample_status(self):
