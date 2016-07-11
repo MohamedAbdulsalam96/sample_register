b8ae3d73bed6e5feea9b34fdc2f7d20fabc5ffa4
sbkolate <kolate.sambhajI@gmail.com>
2016-07-11 12:40:34 +0530

    TRB Session Cleanup

--- sample_register/sample_register/doctype/trb_session/trb_session.js
+++ sample_register/sample_register/doctype/trb_session/trb_session.js
@@ -50,15 +50,10 @@ frappe.ui.form.on("TRB Session", {
 			    var c = d.get_values();
 				var me = this;
 		        var test_list = [];
-		        //get checked 'Test' records
-				
 				$(".frappe-control input:checkbox:checked").each ( function() {
 					test_list.push($(this).attr("name"));
 				});
-
-			// console.log("selectedData",selectedData)
 			console.log("test_list",test_list);
-        //create job card against each sample
 		    frappe.call({
 					method: "start_session",
 					doc: frm.doc,
@@ -74,14 +69,6 @@ frappe.ui.form.on("TRB Session", {
 		'Submit'
 		);
 
-		// var selectedData = [],
-		// selectedIndexes;
-		// selectedIndexes = grid.getSelectedRows();
-		// jQuery.each(selectedIndexes, function (index, value) {
-		// selectedData.push(grid.getDataItem(value));
-		// });
-
-			//set sales order items in popup
 		frappe.call({
 			method: "get_details_from_child_table",
 			type: "GET",
@@ -89,59 +76,28 @@ frappe.ui.form.on("TRB Session", {
 			doc: frm.doc,
 			callback: function(r){
 				if(r.message.get_items){   
-				console.log(r.message.get_items) 
-				// console.log(r.message.get_items[3]["test_type"])                
-                    //remove existing checked checkbox
+				// console.log(r.message.get_items) 
 					$('.frappe-control input:checkbox').removeAttr('checked');
 
 				    html=""
 				    html += '<div class="testCont"  style="max-height: 350px;overflow: auto;overflow-x: hidden;min-height:150px">'
 				    for (var i = 0; i<r.message.get_items.length; i=i+1) {
-				    	// html += "<input type='checkbox' class='select' id='_select' name='"+r.message.get_test_data[i][0]+"' value='"+r.message.get_test_data[i][0]+"'>"+r.message.get_test_data[i][0]+"<br>"
-				    	// html += "<div class='row'>  <div class='col-sm-12'>"
 				    	html += "<label style='font-weight: normal;'><input type='checkbox' class='select' id='_select' name='"
 				    	+r.message.get_items[i]["test_name"]
 				    	+"' value='"+r.message.get_items[i]["test_name"]+"'> "
 				    	+ r.message.get_items[i]["sample_id"] +" ("
 				    	+ r.message.get_items[i]["test_type"] +")"
-
 						r.message.get_items[i]["priority"] ? html += " ("
 						+ r.message.get_items[i]["priority"] +")":""
-
 				    	html += "</label><br>"
-
 				    }
-
 				   	html += '</div>'	
                   	var wrapper = d.fields_dict.test.$wrapper;
                   	wrapper.empty();
 					wrapper.html(html);
-
-
-				 // selected_sample_html= "<p>Select Test to Create Job Card</p>"
-				 //apend selected sample with sample id
-				 // selected_sample_html='<div class="testSelect" style="max-height: 200px;overflow: auto;overflow-x: hidden;min-height:150px">'
-				 // selected_sample_html+="<p>Selected sample to perform Test: </p>"
-				 //  for(r in selectedData){
-			  //      selected_sample_html+="<p>"+selectedData[r]["sampleid"]+"</p>"
-			  //   }
-
-			  //   selected_sample_html += '</div>'	
-				// var wrapper_sample = d.fields_dict.select_test.$wrapper;
-				// wrapper_sample.html(selected_sample_html);
-				 //end of apend seleted sample
-
 				}
 			}
 		});
-
-		// return frappe.call({
-		// 	method: "start_session",
-		// 	doc: frm.doc,
-		// 	callback: function(r, rt){
-		// 		frm.refresh()
-		// 	}
-		// });
 	},
 });
 
