1e74fbeaada7817bd79c2b461897253efffe3256
sbkolate <kolate.sambhajI@gmail.com>
2016-08-04 15:33:47 +0530

    Updated Test with unit

--- sample_register/sample_register/doctype/water_content_test/water_content_test.js
+++ sample_register/sample_register/doctype/water_content_test/water_content_test.js
@@ -28,23 +28,23 @@ cur_frm.fields_dict.lab_equipment_details.grid.get_field("fixed_asset_serial_num
 }
 frappe.ui.form.on('Water Content Test', {
 	validate: function(frm) {
-		var total_drift=0;
-		var total_volume=0;
+		// var total_drift=0;
+		// var total_volume=0;
 		var total_ir=0;
 		var total_length=0;
 		if(frm.doc.water_content_test_details){
 			for(i=0;i<frm.doc.water_content_test_details.length;i++){
 				if(frm.doc.water_content_test_details[i].test__result_status=="Accept"){
-					total_drift += frm.doc.water_content_test_details[i].drift_measured;
-					total_volume += frm.doc.water_content_test_details[i].volume_of_oil;
+					// total_drift += frm.doc.water_content_test_details[i].drift_measured;
+					// total_volume += frm.doc.water_content_test_details[i].volume_of_oil;
 					total_ir += frm.doc.water_content_test_details[i].instrument_reading;
 					total_length +=1;
 				}
 			}
-			frm.set_value("drift_measured",total_drift/total_length)
-			frm.set_value("volume_of_oil",total_volume/total_length)
+			// frm.set_value("drift_measured",total_drift/total_length)
+			// frm.set_value("volume_of_oil",total_volume/total_length)
 			frm.set_value("avg_instrument_reading",total_ir/total_length)
-			frm.set_value("final_result",frm.doc.avg_instrument_reading / (frm.doc.volume_of_oil*frm.doc.drift_measured))
+			frm.set_value("final_result",frm.doc.avg_instrument_reading / (frm.doc.volume_of_oil*frm.doc.density_of_oil))
 
 		}
 
