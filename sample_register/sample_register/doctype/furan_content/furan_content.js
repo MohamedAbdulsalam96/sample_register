d78febfe8ea4e3814c52f9ec02a265fe302a36d7
sbkolate <kolate.sambhajI@gmail.com>
2016-11-24 16:26:06 +0530

    Updated Furan and DGA

--- sample_register/sample_register/doctype/furan_content/furan_content.js
+++ sample_register/sample_register/doctype/furan_content/furan_content.js
@@ -4,5 +4,19 @@
 frappe.ui.form.on('Furan Content', {
 	refresh: function(frm) {
 
-	}
+	},
+	onload: function(frm){
+		if(frm.doc.run_one.length <2){
+			var gases = { "furans" :["5H2F","2FUR","2 FURALDEHYDE","2ACF","5M2F"], 
+						"gas_name": ["Oxygen","Nitrogen","Carbon","Hydrogen"," Carbon Monoxide","Methane",
+						"Ethane","Ethylene","Acetelene","Propane","Propylene"]}
+
+			$.each(gases["furans"], function(i, d) {
+                var row = frappe.model.add_child(cur_frm.doc, "Furan Content Test Details", "run_one");
+                row.furans = gases["furans"][i];
+                // row.gas_name = gases["gas_name"][i];
+            })
+            refresh_field("run_one");
+		}
+	},
 });
